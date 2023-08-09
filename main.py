85b21a64286558a2a0ac0cb3e90f3401a0d552a8
MA-Mahmudi <ma.mahmudi18@outlook.com>
2023-08-09 23:20:23 +0330

    main.py

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ from datetime import datetime as dt
 
 
 def sendTimingMessage():
+    print("Starting...")
+    app.run()
+    print("Started successfully")
     now = dt.now().strftime("%H:%M")
     # enter the times you want the message to be sent. The time format must be 24hrs
-    sendTimes = ["6:00", "9:15", "9:30", "9:40", "22:30"]
-    for i in sendTimes:
-        if i == now:
-            app.send_message()
+    send_times = ["6:00", "9:15", "9:30", "9:40", "22:30", "22:41"]
+    # for i in send_times:
+    #     if i == str(now):
+    # app.send_message(chat_id=)
+
+
+sendTimingMessage()
